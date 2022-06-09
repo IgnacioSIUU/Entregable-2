57a8bd5de7f85ebad1d1419a8332005835542cda
JesuMorales <104163800+JesuMorales@users.noreply.github.com>
2022-06-09 14:38:59 -0400

    final corregido

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -3,7 +3,7 @@ from .views import home, form_arte, form_mod_arte, form_del_arte
 
 urlpatterns = [
     path('',home,name="home"),
-    path('form-arte', form_arte, name="form_arte"),
+    path('form_arte', form_arte, name="form_arte"),
     path('form-mod-arte/<id>', form_mod_arte, name="form_mod_arte"),
     path('form-del-arte/<id>', form_del_arte, name="form_del_arte"),
 ]
